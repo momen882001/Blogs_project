7554b1603b5635af2ded62bd3a00c8a36b2e34a9
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2022-09-03 17:32:59 +0200

    handel token

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
-const { compare } = require("bcrypt");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+
+const createToken = (payload) => {
+  const token = jwt.sign(payload, "temporary secret for testing");
+  return token;
+};
 
 router.post("/login", async (req, res) => {
-  const user = req.body;
+  let user = req.body;
   const exist = await User.findOne({ email: user.email });
 
   if (!exist) return res.status(400).json({ err: "Invalid email or password" });
@@ -14,7 +20,14 @@ router.post("/login", async (req, res) => {
   if (!validPass) {
     return res.status(400).json({ err: "Invalid email or password" });
   }
-  res.status(200).send(exist);
+  user = exist._doc;
+
+  const token = createToken({ id: user.id });
+  res.header("auth-token", token);
+
+  res
+    .status(200)
+    .send({ ..._.pick(user, ["name", "email", "birthdate"]), token });
 });
 
 module.exports = router;
