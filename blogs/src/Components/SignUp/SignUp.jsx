7e10c6fe033840dcdc3b709e969c8d91d4be4226
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-09-02 17:44:53 -0700

    SignUp Design part 1

--- blogs/src/Components/SignUp/SignUp.jsx
+++ blogs/src/Components/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React , {useState} from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from '../API/axios'
+import './SignUp.css'
 
 function SignUp() {
 
@@ -32,33 +33,38 @@ function SignUp() {
     return (
         <div className="signup">
             <form className="signup-form" onSubmit={onSubmit}>
+            <h1 className="h1-signup">SignUp</h1>
 
-                <div className="row">
+                <div className="signup-con">
+
+                <div className="row-signup">
                     <label htmlFor="">Name :</label>
                     <input autoFocus type="text" required placeholder="Write your name" value={name} onChange={(e) => setName(e.target.value)}  />
                 </div>
 
-                <div className="row">
+                <div className="row-signup">
                     <label htmlFor="">Email :</label>
                     <input  type="email" required placeholder="Write your email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
-                <div className="row">
+                <div className="row-signup">
                     <label htmlFor="">Password :</label>
                     <input  type="password" required placeholder="Write your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
 
-                <div className="row">
+                <div className="row-signup">
                     <label htmlFor="">Confirm Password :</label>
                     <input  type="password" required placeholder="Write your password again" value={confirm_password} onChange={(e) => setConfirmPassword(e.target.value)} />
                 </div>
 
-                <div className="row">
+                <div className="row-signup">
                     <label htmlFor="">Birthdate :</label>
                     <input type="date" required placeholder="Write your Birthdate" value={birthdate} onChange={(e) => setBirthdate(e.target.value)}/>
                 </div>
 
-                <button type="submit">SignUp</button>
+                </div>
+
+                <button type="submit" className="signup-btn">SignUp</button>
 
             </form>
         </div>
