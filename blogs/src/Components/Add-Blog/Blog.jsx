769a6509fccb090a48379115e36d7d24e25d4a7a
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2022-10-21 04:34:10 -0700

    edit-blog component

--- blogs/src/Components/Add-Blog/Blog.jsx
+++ blogs/src/Components/Add-Blog/Blog.jsx
@@ -1,6 +1,6 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 import './Blog.css'
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import axios from '../API/axios'
 import imgLeft from '../assets/Add files-amico.png'
 import NavBar from '../navBar/NavBar'
@@ -13,78 +13,69 @@ function Blog() {
     const navigate = useNavigate();
 
     const onSubmit = (e) => {
-        console.log(category,title,body)
+        console.log(category, title, body)
         e.preventDefault()
 
         axios.post('/blog', {
-           category,
-           title,
-           body
-        },{
-            headers:{
-                "auth-token" : localStorage.getItem("auth-token")
+            category,
+            title,
+            body
+        }, {
+            headers: {
+                "auth-token": localStorage.getItem("auth-token")
             }
         }).then((response) => {
             console.log("posting data", response)
             navigate('/sidebar/user');
         }).catch((err) => {
-           setErr(err.response.data.err)
+            setErr(err.response.data.err)
         })
     };
 
     return (
         <>
-         <NavBar/>
-        <div className="login">
-<section className="side" style={{padding:"0", margin:"0"}}>
-        <img src= {imgLeft} alt="" size="2x" style={{padding:"0", margin:"0"}}/>
-    </section>
+            <NavBar />
+            <div className="login">
+                <section className="side" style={{ padding: "0", margin: "0" }}>
+                    <img src={imgLeft} alt="" size="2x" style={{ padding: "0", margin: "0" }} />
+                </section>
 
-            <section className='main'>
-            <div className="login-container">
-            <p className="title" >Add-Blog</p>
-            <div className="separator" style={{margin: "5px", width:"150px",marginTop: "-7px",marginBottom: "20px", backgroundColor:"rgb(64 123 255)"}}></div>
+                <section className='main'>
+                    <div className="login-container">
+                        <p className="title" >Add-Blog</p>
+                        <div className="separator" style={{ margin: "5px", width: "150px", marginTop: "-7px", marginBottom: "20px", backgroundColor: "rgb(64 123 255)" }}></div>
 
-            <form className="blog-form" onSubmit={onSubmit}>
-               <div className="row-blog">
-               <label htmlFor="blog-names">Choose Category:</label>
-                <select value={category} onChange={(e) => setCategory(e.target.value)} name="blog-names" id="blog-names">
-                    <option  value="" disabled selected hidden></option>
-                    <option value="Travel">Travel</option>
-                    <option value="Shopping">Shopping</option>
-                    <option value="Development">Development</option>
-                    <option value="Sports">Sports</option>
-                    <option value="Art">Art</option>
-                </select>
-               </div>
+                        <form className="blog-form" onSubmit={onSubmit}>
+                            <div className="row-blog">
+                                <label htmlFor="blog-names">Choose Category:</label>
+                                <select value={category} onChange={(e) => setCategory(e.target.value)} name="blog-names" id="blog-names">
+                                    <option value="" disabled selected hidden></option>
+                                    <option value="Travel">Travel</option>
+                                    <option value="Shopping">Shopping</option>
+                                    <option value="Development">Development</option>
+                                    <option value="Sports">Sports</option>
+                                    <option value="Art">Art</option>
+                                </select>
+                            </div>
 
-               <div className="row-blog">
-               <label>Title :</label>   
-               <input required value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Write your title" />
-               </div>
+                            <div className="row-blog">
+                                <label>Title :</label>
+                                <input required value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Write your title" />
+                            </div>
 
-               <div className="row-blog">
-               <label>Body :</label>
-               <textarea required value={body} onChange={(e) => setBody(e.target.value)} id="textArea-blog" name="textArea-blog" rows="4" cols="50"  >
-                    
-                </textarea>
-               </div>
-               {err ? <p className="error-blog">{err}</p> : null}
+                            <div className="row-blog">
+                                <label>Body :</label>
+                                <textarea required value={body} onChange={(e) => setBody(e.target.value)} id="textArea-blog" name="textArea-blog" rows="4" cols="50"  >
 
-               <button className="blog-btn" type="submit">Save</button>
+                                </textarea>
+                            </div>
+                            {err ? <p className="error-blog">{err}</p> : null}
 
-               {/* <div className="other">
-                <Link to="/">
-                Home
-                  </Link>
-                  <Link to="/User">
-                  My-Blogs
-                  </Link>
-                </div> */}
-            </form>
+                            <button className="blog-btn" type="submit">Save</button>
+                        </form>
+                    </div>
+                </section>
             </div>
-            </section>
-        </div>
         </>
     )
 }
--- blogs/src/Components/Edit-Blog/Edit.jsx
+++ blogs/src/Components/Edit-Blog/Edit.jsx
@@ -1,7 +1,9 @@
-import React , {useState,useEffect} from 'react'
-import { useNavigate,Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useNavigate, Link } from "react-router-dom";
 import axios from '../API/axios'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import NavBar from '../navBar/NavBar'
+import imgRight from '../assets/Post-pana.png'
 
 
 function Edit() {
@@ -11,95 +13,93 @@ function Edit() {
     const [body, setBody] = useState('');
     const [err, setErr] = useState('');
     const navigate = useNavigate();
-    const {blog_id} = useParams()
+    const { blog_id } = useParams()
 
 
     useEffect(() => {
         axios.get(`/blog/${blog_id}`)
-        .then((response) => {
-            console.log("Getting data", response.data)
-            setTitle(response.data.title)
-            setCategory(response.data.category)
-            setBody(response.data.body)
-        }).catch((err) => {
-            console.log(err)
-        })
-     }, [blog_id])
+            .then((response) => {
+                console.log("Getting data", response.data)
+                setTitle(response.data.title)
+                setCategory(response.data.category)
+                setBody(response.data.body)
+            }).catch((err) => {
+                console.log(err)
+            })
+    }, [blog_id])
 
-     
+
 
 
     const edit = (e) => {
-        console.log(category,title,body)
+        console.log(category, title, body)
         e.preventDefault()
 
         axios.put(`/blog/${blog_id}`, {
-           category,
-           title,
-           body
-        },{
-            headers:{
-                "auth-token" : localStorage.getItem("auth-token")
+            category,
+            title,
+            body
+        }, {
+            headers: {
+                "auth-token": localStorage.getItem("auth-token")
             }
         }).then((response) => {
             console.log("Editting data", response)
             navigate('/sidebar/user');
         }).catch((err) => {
-           setErr(err.response.data.err)
+            setErr(err.response.data.err)
         })
     };
 
-    
+
 
     return (
-        <div className="blog">
-            <form className="blog-form" onSubmit={edit}>
+        <>
+            <NavBar />
+            <div className="login">
 
-                <h1 className="h1-blog">Edit Blog</h1>
 
-               <div className="row-blog">
-               <label htmlFor="blog-names">Choose Category:</label>
-                <select value={category} onChange={(e) => setCategory(e.target.value)} name="blog-names" id="blog-names">
-                    <option  value="" disabled selected hidden></option>
-                    <option value="Travel">Travel</option>
-                    <option value="Shopping">Shopping</option>
-                    <option value="Development">Development</option>
-                    <option value="Sports">Sports</option>
-                    <option value="Art">Art</option>
-                </select>
-               </div>
+                <section className='main'>
+                <div className="login-container">
+                <p className="title" >Edit-Blog</p>
+                <div className="separator" style={{ margin: "5px", width: "150px", marginTop: "-7px", marginBottom: "20px", backgroundColor: "rgb(64 123 255)" }}></div>
 
-               <div className="row-blog">
-               <label>Title :</label>   
-               <input required value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Write your title" />
-               </div>
+                <form className="blog-form" onSubmit={edit}>
+                    <div className="row-blog">
+                        <label htmlFor="blog-names">Choose Category:</label>
+                        <select value={category} onChange={(e) => setCategory(e.target.value)} name="blog-names" id="blog-names">
+                            <option value="" disabled selected hidden></option>
+                            <option value="Travel">Travel</option>
+                            <option value="Shopping">Shopping</option>
+                            <option value="Development">Development</option>
+                            <option value="Sports">Sports</option>
+                            <option value="Art">Art</option>
+                        </select>
+                    </div>
 
-               <div className="row-blog">
-               <label>Body :</label>
-               <textarea required value={body} onChange={(e) => setBody(e.target.value)} id="textArea-blog" name="textArea-blog" rows="4" cols="50"  >
-                    
-                </textarea>
-               </div>
-               {err ? <p className="error-blog">{err}</p> : null}
+                    <div className="row-blog">
+                        <label>Title :</label>
+                        <input required value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Write your title" />
+                    </div>
 
-               <button className="blog-btn" type="submit">Edit</button>
+                    <div className="row-blog">
+                        <label>Body :</label>
+                        <textarea required value={body} onChange={(e) => setBody(e.target.value)} id="textArea-blog" name="textArea-blog" rows="4" cols="50"  >
 
-               <div  className="links-contain">
-                    
-                    <Link to="/" style={{textDecorationColor:"rgba(11,83,148,1)"}}>
-                    <p className="p-login">Home</p>
-                    </Link>
-                    
-                    
-                    <Link to="/User" style={{textDecorationColor:"rgba(11,83,148,1)"}}>
-                    <p className="p-login">My-Blogs</p>
-                    </Link>
-                    
+                        </textarea>
+                    </div>
+                    {err ? <p className="error-blog">{err}</p> : null}
+
+                    <button className="blog-btn" type="submit">Edit</button>
+                </form>
                 </div>
+                </section>
 
-
-            </form>
-        </div>
+                <section className="side" style={{ padding: "0", margin: "0" }}>
+                    <img src={imgRight} alt="" size="2x" style={{ padding: "0", margin: "0" }} />
+                </section>
+            </div>
+        </>
     )
 }
 
